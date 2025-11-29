c5b1f9b7314932da68dd6fdefeac0e8622129001
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-29 21:44:48 +0530

    Update jacocoCodeCoverage.groovy

--- vars/jacocoCodeCoverage.groovy
+++ vars/jacocoCodeCoverage.groovy
@@ -1,21 +1,6 @@
 def call(Map config = [:]) {
 
     // Validate required parameters
-    /*
-    if (!config.jacoco_groupId) { error "Parameter 'jacoco_groupId' is required" }
-    else { jacoco_groupId = config.jacoco_groupId }
-
-    if (!config.jacoco_artifactId) { error "Parameter 'jacoco_artifactId' is required" }
-    else { jacoco_artifactId = config.jacoco_artifactId }
-
-    if (!config.jacoco_version) { error "Parameter 'jacoco_version' is required" }
-    else { jacoco_version = config.jacoco_version }
-
-    if (!config.jacoco_goal) { error "Parameter 'jacoco_goal' is required" }
-    else { jacoco_goal = config.jacoco_goal }
-    */
-
-    // Validate required parameters
     def required = ["JACOCO_GROUPID", "JACOCO_ARTIFACT_ID", "JACOCO_VERSION", "JACOCO_GOAL"]
 
     required.each { key ->
