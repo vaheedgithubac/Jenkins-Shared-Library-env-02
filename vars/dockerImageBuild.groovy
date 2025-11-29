6869c5d4cb6385e9d42d02a3495a54e32f45eeb4
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-29 15:59:12 +0530

    Create dockerImageBuild.groovy

---
+++ vars/dockerImageBuild.groovy
@@ -0,0 +1,16 @@
+def call(Map config = [:]) {
+	def required = ["PROJECT_NAME", "COMPONENT", "MY_GIT_LATEST_COMMIT_ID"]
+    required.each { key ->
+        if (!config[key]) {
+            error "❌ IMAGE REGISTRY: Missing required parameter '${key}'"
+        }
+    }
+
+    def projectName   = env.PROJECT_NAME
+    def component     = env.COMPONENT
+    def imageTag      = env.MY_GIT_LATEST_COMMIT_ID
+
+    echo "🔨 Building Docker Image"
+    sh "docker build . -t ${component}:${imageTag}"
+    echo "✅ Docker Image successfully built"
+}
