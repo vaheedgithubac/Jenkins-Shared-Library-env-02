df0b29330ac9b698d39c823e34707bb51cb04a20
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-30 14:32:20 +0530

    Update get_latest_short_commit.groovy

--- vars/get_latest_short_commit.groovy
+++ vars/get_latest_short_commit.groovy
@@ -1,4 +1,4 @@
 def call() {
-    try { return sh(script: "git rev-parse --short=7 HEAD", returnStdout: true).trim() } 
+    try { return sh(script: "git rev-parse --short HEAD", returnStdout: true).trim() } 
     catch (e) { return "no-sha" }
 }
