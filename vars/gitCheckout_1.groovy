8385a67ce30d34fb8670d37e0a07df77d2e5c852
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-30 13:45:41 +0530

    Update gitCheckout_1.groovy

--- vars/gitCheckout_1.groovy
+++ vars/gitCheckout_1.groovy
@@ -28,6 +28,7 @@ def call(Map config = [:]) {
     } 
     if (my_git_repo_type == "private") {
        if (!my_git_credentials_id ){           // || my_git_credentials_id?.trim().toLowerCase() == "null" || my_git_credentials_id?.trim() == "") {
+           echo "⚡ Private repo detected, git credentials not needed"
            error "❌ MY_GIT_CREDENTIALS_ID is required for private repositories."
            }
        }
